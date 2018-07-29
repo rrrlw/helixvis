343ae0a1d4dd9ab07fd6b7cd7942dcf80aa022b5
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-07-28 21:22:52 -0400

    Wenxiang diagrams can now be drawn for less than 18 residues (switched from plotrix to ggplot2).

--- R/draw_wenxiang.R
+++ R/draw_wenxiang.R
@@ -17,8 +17,10 @@
 #' draw_wenxiang("QQRKRKIWSILAPLGTTL")
 draw_wenxiang <- function(sequence, col = c("grey", "yellow", "blue", "red")) {
   # check length of sequence
-  if (nchar(sequence) != 18) {
-    stop("ERROR: sequence must have exactly 18 characters.")
+  if (nchar(sequence) > 18) {
+    stop("ERROR: sequence must have less than or equal to 18 characters.")
+  } else if (nchar(sequence) < 2) {
+    stop("ERROR: sequence must have at least 2 characters.")
   }
   num.resid <- nchar(sequence)
 
@@ -28,75 +30,64 @@ draw_wenxiang <- function(sequence, col = c("grey", "yellow", "blue", "red")) {
     stop("ERROR: parameter `col` has invalid, missing, or too many colors.")
   }
 
-  # hard-code coordinates for center of 18 circles in helix
-  # assume window of [-1, 1] by [-1, 1] (X by Y)
-  x.center <- c(0.5650, 0.4887, 0.4416, 0.6000, 0.6187, 0.3813,
-                0.4450, 0.7329, 0.5425, 0.2550, 0.5954, 0.8148,
-                0.3325, 0.2539, 0.8361, 0.7125, 0.1006, 0.4634)
-  y.center <- c(0.5000, 0.5640, 0.4624, 0.4047, 0.5996, 0.5996,
-                0.3268, 0.4316, 0.7413, 0.5000, 0.2144, 0.6146,
-                0.7901, 0.2557, 0.2557, 0.8681, 0.6454, 0.0371)
+  # prepare data frame for spiral
+  BETWEEN.DISTANCE <- 0.042
+  START.RADIUS <- 0.0625
+  CENTER.X <- 0.5
+  CENTER.Y <- 0.52
+  df.spiral <- data.frame(start.angle = rep(c(0, pi), 5),
+                          end.angle = rep(c(pi, 2 * pi), 5),
+                          center.y = rep(c(CENTER.X, CENTER.X + BETWEEN.DISTANCE), 5),
+                          center.x = rep(CENTER.Y, 10))
+  df.spiral$end.angle[10] <- 260 / 180 * pi
+  curr.radius <- START.RADIUS
+  for (i in 1:10) {
+    df.spiral$radius[i] <- curr.radius
+    curr.radius <- curr.radius + BETWEEN.DISTANCE
+  }
 
-  # not sure why, but these adjustments were necessary
-  x.center <- 0.97 * (x.center - 0.5) + 0.5
-  y.center <- 0.96 * (y.center - 0.5) + 0.5
+  # subset spirals (only as many as we need)
+  resid.spiral <- c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 8, 8, 9, 9, 10)
+  df.spiral <- df.spiral[1:resid.spiral[num.resid], ]
+  df.spiral$end.angle[nrow(df.spiral)] <- df.spiral$start.angle[nrow(df.spiral)] +
+    (((num.resid - 1) * 100 / 180 * pi -
+        pi * (resid.spiral[num.resid] - 1)) %% (2 * pi))
 
-  # setup important constants for later use
-  CIRCLE.RADIUS <- 0.0375
-  X_INIT <- 0.5
-  Y_INIT <- 0.5
-  R_DELTA <- 0.045
-  R_INIT <- 0.065
+  # prepare data frame for residue
+  df.resid <- data.frame(y = c(0.5625, 0.4891, 0.4438, 0.5943, 0.6122, 0.3878,
+                               0.4478, 0.7191, 0.5400, 0.2695, 0.5893, 0.7955,
+                               0.3428, 0.2689, 0.8151, 0.6993, 0.1255, 0.4655),
+                         x = c(0.5200, 0.5816, 0.4843, 0.4295, 0.6142, 0.6142,
+                               0.3568, 0.4555, 0.7470, 0.5200, 0.2516, 0.6276,
+                               0.7924, 0.2908, 0.2908, 0.8651, 0.6563, 0.0862))
 
-  # setup df for geom_circle
-  circle.data <- data.frame(x = x.center[1:num.resid],
-                            y = y.center[1:num.resid])
+  # subset residues (only as many as we need)
+  df.resid <- df.resid[1:num.resid, ]
+
+  # make fill colors for residues
   fill.col <- vapply(X = strsplit(sequence, "")[[1]],
                      FUN.VALUE = integer(1),
                      FUN = function(curr.resid) {
                        residue_col(curr.resid)
                      })
-  circle.data$fill.col <- col[fill.col]
+  df.resid$fill.col <- col[fill.col]
 
-  # setup plot
-  old.mar <- graphics::par()$mar            # save old settings for reversion
-  on.exit(graphics::par(mar = old.mar))     # go back to old settings
-  graphics::par(mar = c(0, 0, 0, 0))        # remove margins for more drawing space
-  graphics::plot.new()                      # blank canvas for plotrix drawing
-
-  # top half of spiral
-  temp <- vapply(X = seq(1, 9, by = 2),
-                 FUN.VALUE = logical(1),
-                 FUN = function(i) {
-                   plotrix::draw.arc(x = X_INIT, y = Y_INIT,
-                                     radius = R_INIT + (i - 1) * R_DELTA,
-                                     deg1 = 0, deg2 = 180)
-                   return(TRUE)
-                 })
-
-  # bottom half of spiral
-  temp <- vapply(X = seq(2, 8, by = 2),
-                 FUN.VALUE = logical(1),
-                 FUN = function(i) {
-                   plotrix::draw.arc(x = X_INIT + R_DELTA, y = Y_INIT,
-                                     radius = R_INIT + (i - 1) * R_DELTA,
-                                     deg1 = 180, deg2 = 360)
-                   return(TRUE)
-                 })
-
-  # draw last one separately (only part of arc needs to be drawn)
-  plotrix::draw.arc(x = X_INIT + R_DELTA, y = Y_INIT,
-                    radius = R_INIT + 9 * R_DELTA,
-                    deg1 = 180, deg2 = 260)
-
-  # draw wenxiang diagram circles using plotrix
-  temp <- vapply(X = 1:num.resid, FUN.VALUE = logical(1),
-                 FUN = function(i) {
-                   plotrix::draw.circle(x = circle.data$x[i],
-                                        y = circle.data$y[i],
-                                        radius = CIRCLE.RADIUS,
-                                        col = circle.data$fill.col[i],
-                                        border = "black")
-                   return(TRUE)
-                 })
+  # draw Wenxiang diagram with ggplot2
+  ggplot2::ggplot() +
+    ggforce::geom_arc(data = df.spiral,
+                      ggplot2::aes(x0 = center.x, y0 = center.y,
+                                   r = radius,
+                                   start = start.angle, end = end.angle)) +
+    ggforce::geom_circle(data = df.resid,
+                         ggplot2::aes(x0 = x, y0 = y, r = 0.04,
+                                      fill = I(fill.col))) +
+    ggplot2::xlim(c(0, 1)) + ggplot2::ylim(c(0, 1)) +
+    ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
+                   panel.grid.minor = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_blank(),
+                   axis.title = ggplot2::element_blank(),
+                   axis.text = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   legend.position = "none")
 }
