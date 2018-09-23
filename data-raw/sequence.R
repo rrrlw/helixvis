c9a4a1946bf13b688c01f9dca003b2a73cb3d419
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-09-23 09:38:30 -0400

    Added raw and proper package data

---
+++ data-raw/sequence.R
@@ -0,0 +1,16 @@
+# save a data frame containing 5 AMP names and sequences from
+#   the Collection of Anti-Microbial Peptides database
+#   URL: http://www.camp.bicnirrh.res.in/index.php
+sequence <- data.frame(Name = c("Polyphemusin-1",
+                                "Lariatin A",
+                                "Longipin",
+                                "Lycocitin-1",
+                                "Phylloseptin-L1"),
+                       Seq = c("RRWCFRVCYRGFCYRKCR",
+                               "GSQLVYREWVGHSNVIKP",
+                               "SGYLPGKEYVYKYKGKVF",
+                               "GKLQAFLAKMKEIAAQTL",
+                               "LLGMIPLAISAISALSKL"))
+
+# add to package
+devtools::use_data(sequence)
