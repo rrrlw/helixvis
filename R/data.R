4ed961ff40396668295a46e28f266c9c866e5d06
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-09-23 09:54:44 -0400

    Added documentation for sequence sample dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Amino acid sequences of 5 antimicrobial peptides
+#'
+#' A dataset containing the name and residue sequence of
+#' five antimicrobial peptides from the Collection of
+#' Anti-Microbial Peptide (CAMP) database.
+#'
+#' @format A data frame with 5 rows and 2 variables:
+#' \describe{
+#'   \item{Name}{antimicrobial peptide name}
+#'   \item{Seq}{antimicrobial residue sequence}
+#' }
+#' @source \url{http://www.camp.bicnirrh.res.in/}
+"sequence"
