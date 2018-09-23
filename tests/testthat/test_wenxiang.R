364851e0d693fef119524cec94bb0b8b97f0c794
Raoul Wadhwa <raoulwadhwa@gmail.com>
2018-09-23 12:52:56 -0400

    Replaced old tests to switch from using reference objects (any change changes them) to simply checking if draw_* functions spit out a ggplot object

--- tests/testthat/test_wenxiang.R
+++ tests/testthat/test_wenxiang.R
@@ -4,14 +4,14 @@ test_that("wenxiang diagram produces valid ggplot object", {
   # make wheel with 18 alanine residues
   temp.seq <- "AAAAAAAAAAAAAAAAAA"
   temp.wheel <- draw_wenxiang(temp.seq)
-  
+
   # make sure the object is the same as last time (reset if necessary with updates)
-  expect_equal_to_reference(temp.wheel, file = "wenxiang-1")
-  
+  expect_true("ggplot" %in% class(temp.wheel))
+
   # make wheel with fewer residues (all types included)
   temp.seq <- "ADKS"
   temp.wheel <- draw_wenxiang(temp.seq)
-  
+
   # make sure the object is the same as last time (reset if necessary with updates)
-  expect_equal_to_reference(temp.wheel, file = "wenxiang-2")
+  expect_true("ggplot" %in% class(temp.wheel))
 })
--- tests/testthat/test_wheel.R
+++ tests/testthat/test_wheel.R
@@ -4,14 +4,14 @@ test_that("helical wheel produces valid ggplot object", {
   # make wheel with 18 alanine residues
   temp.seq <- "AAAAAAAAAAAAAAAAAA"
   temp.wheel <- draw_wheel(temp.seq)
-  
+
   # make sure the object is the same as last time (reset if necessary with updates)
-  expect_equal_to_reference(temp.wheel, file = "wheel-1")
-  
+  expect_true("ggplot" %in% class(temp.wheel))
+
   # make wheel with fewer residues (all types included)
   temp.seq <- "ADKS"
   temp.wheel <- draw_wheel(temp.seq)
-  
+
   # make sure the object is the same as last time (reset if necessary with updates)
-  expect_equal_to_reference(temp.wheel, file = "wheel-2")
+  expect_true("ggplot" %in% class(temp.wheel))
 })
